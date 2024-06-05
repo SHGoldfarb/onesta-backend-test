9ce95e37caaad6bad010ac9a26e0ddde94e7076d
Samuel Goldfarb <samuelghau@gmail.com>
2024-06-05 10:08:01 -0400

    feat: add POST endpoint for /fruits

--- src/routes/fruits.ts
+++ src/routes/fruits.ts
@@ -2,12 +2,29 @@ import express, { Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
 import { Fruit } from "../models/fruit.js";
 
-export const fruitsRouter = express.Router();
+const router = express.Router();
 
-fruitsRouter.get("/fruits", async (request: Request, response: Response) => {
+router.get("/fruits", async (request: Request, response: Response) => {
   const fruits = await Fruit.findAll();
 
   return response
     .status(StatusCodes.OK)
     .json({ total_fruits: fruits.length, fruits });
 });
+
+router.post("/fruits", async (request: Request, response: Response) => {
+  const { name } = request.body;
+  console.log(request);
+
+  if (!name) {
+    return response
+      .status(StatusCodes.BAD_REQUEST)
+      .json({ error: `Please provide the name of the fruit` });
+  }
+
+  const fruit = await Fruit.create({ name });
+
+  return response.status(StatusCodes.CREATED).json({ fruit });
+});
+
+export const fruitsRouter = router;
